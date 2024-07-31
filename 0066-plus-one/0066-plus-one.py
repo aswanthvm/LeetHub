6694225ad18dd6234915465476c4bb9a139df84e
Aswanth V M <150995136+aswanthvm@users.noreply.github.com>
2024-07-31 14:43:52 +0530

    Time: 19 ms (28.17%), Space: 11.7 MB (26.78%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,3 +1,23 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        return list(map(int, str(int(''.join([str(i) for i in digits]))+1)))
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        i= len(digits)-1
+        while(digits[i] == 9 and i>=0):
+            if (i == 0):
+                tempArray = [0]*(len(digits)+1)
+                tempArray[0] = 1
+                return tempArray
+            digits[i] = 0
+            i-=1
+        digits[i] += 1
+        return digits
+        
+
+
+        
+
+            
+        
