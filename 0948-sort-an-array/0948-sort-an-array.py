a4760f223daf2891b99d8aed7a0ae0ced661c0cd
Aswanth V M <150995136+aswanthvm@users.noreply.github.com>
2025-08-10 22:56:15 +0530

    Time: 9534 ms (5.03%), Space: 29.4 MB (16.3%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,5 +1,26 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
+        self.quickSort(nums, 0, len(nums) - 1)
         return nums
-        
+
+    def quickSort(self, arr: list[int], low: int, high: int):
+        if low >= high:
+            return
+
+        start = low
+        end = high
+        pivot = arr[low + (high - low) // 2]  # A more robust way to find the middle index
+
+        while start <= end:
+            while arr[start] < pivot:
+                start += 1
+            while arr[end] > pivot:
+                end -= 1
+
+            if start <= end:
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+                end -= 1
+
+        self.quickSort(arr, low, end)
+        self.quickSort(arr, start, high)
