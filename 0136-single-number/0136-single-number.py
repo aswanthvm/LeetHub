c6d259dd748b1163b08c6ab038d9ceba7f28bfc9
Aswanth V M <150995136+aswanthvm@users.noreply.github.com>
2025-10-18 00:34:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (94.08%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        total_sum = 0
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
 
-        for i in nums:
-            total_sum = total_sum ^ i
-            
-        return total_sum
         
