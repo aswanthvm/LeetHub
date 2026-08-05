88cf9bd9b39a2afdd549a1077078670a609a2d80
Aswanth V M <150995136+aswanthvm@users.noreply.github.com>
2026-08-05 14:30:32 +0530

    Time: 3 ms (86.15%), Space: 20.6 MB (49.73%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = [0]*numCourses
+        adj = [[]for i in range(numCourses)]
+        for src, dst in prerequisites:
+            indegree[dst] += 1
+            adj[src].append(dst)
+        q = deque()
+        for n in range(numCourses):
+            if indegree[n] == 0:
+                q.append(n)
+        while q:
+            node = q.popleft()
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        return sum(indegree) == 0
+
+        
